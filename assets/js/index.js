73ed468fbfc72df7b5b9ec6bebfa7b5a2901b505
Ben Kelly <benskelly10@gmail.com>
2021-02-05 20:24:21 +0000

    Add filter panel to narrow songs returned
    - Add hidden filters that can be active or inactive
    - Populate randomSongs array with 5 random songs on form submit

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,13 +3,43 @@ $(document).ready(function() {
     $("#random-songs-form").on("submit", function(e) {
         e.preventDefault();
         form = $(this);
-        
-        var urlParams = {
-            "genre": form.find("#random-songs-genre").val(),
-            "artist": form.find("#random-songs-artist").val(),
-            "hasLyrics": form.find("#random-songs-lyrics-check").prop("checked") ? 1 : 0
-        };
+        var activeFilters = form.find(".form-group:not(.inactive)");
+        var formData = {};
 
-        console.log(urlParams);
+        activeFilters.each(function() {
+            let field = $(this).data("field");
+            if (field == "hasLyrics") {
+                var value = $(this).find(`#random-songs-${field}`).prop("checked") ? 1 : 0;
+            } else {
+                var value = $(this).find(`#random-songs-${field}`).val();
+            }
+            formData[field] = value;
+        });
+
+        console.log(formData);
+        getRandomSongs(formData);
+    });
+
+    $("button.filter-btn").on("click", function() {
+        var active = !($(this).data("active"));
+        $(this).data("active", active);
+        // Split name at first "-" to get filter field
+        var filterField = $(this).attr("name").split("-")[0];
+
+        if (active) {
+            $(this).removeClass("btn-outline-success");
+            $(this).addClass("btn-outline-danger");
+            $(this).find(".fa-check").hide();
+            $(this).find(".fa-times").show();
+
+            $(`[data-field="${filterField}"]`).removeClass("inactive");
+        } else {
+            $(this).removeClass("btn-outline-danger");
+            $(this).addClass("btn-outline-success");
+            $(this).find(".fa-times").hide();
+            $(this).find(".fa-check").show();
+
+            $(`[data-field="${filterField}"]`).addClass("inactive");
+        }
     });
 });
--- assets/js/randomSongs.js
+++ assets/js/randomSongs.js
@@ -1,10 +1,77 @@
 var baseURL = 'https://api.musixmatch.com/ws/1.1/';
 var apiKey = '8ebb100f8dbd94b032393de929156869';
+var formData;
+var url;
+var randomSongs = [];
 
-function getRandomSongs() {
-        
-    var url = `https://api.musixmatch.com/ws/1.1/track.search?format=json&f_lyrics_language=en&f_has_lyrics=1&s_track_rating=desc&quorum_factor=1&page_size=1&page=1&apikey=8ebb100f8dbd94b032393de929156869`;
-    $.get(url, function(data){
-        console.log(data);
+function getRandomSongs(formData) {
+    formData = formData
+    randomSongs = [];
+    url = 'https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=initialJsonpCallback&f_lyrics_language=en&s_track_rating=desc';
+
+    if (formData.hasLyrics) {
+        var hasLyrics = `&f_has_lyrics=${formData.hasLyrics}`;
+        url += hasLyrics;
+    }
+    if (formData.genre) {
+        var genre = `&f_music_genre_id=${formData.genre}`;
+        url += genre;
+    }
+    if (formData.artist) {
+        var artist = `&q_artist=${formData.artist}`;
+        url += artist;
+    }
+
+    $.ajax({
+        url: url + '&quorum_factor=1&page_size=1&page=1&apikey=8ebb100f8dbd94b032393de929156869',
+        type: "GET",
+        dataType: "jsonp",
+        jsonpCallback: "initialJsonpCallback"
     });
 };
+
+function initialJsonpCallback(json) {
+    var available = json.message.header.available;
+    // Initial logic on how to page results from (https://github.com/kodie/rnd-song/blob/master/index.js)
+
+    if (available > 0) {
+        var pages = Math.ceil(available / 100);
+        window.retrieveSongcount = 5;
+        window.pages = pages;
+        window.songsRetrieved = 0;
+        randomSongList();
+        // for(let i = 0; i < 5; i++) {
+        // }
+    } else {
+        alert("No songs found with those details. Try again.");
+    }
+};
+
+function randomSongList() {
+    debugger;
+    var page = Math.floor( (Math.random() * window.pages) + 1 );
+    $.ajax({
+        url: url + `&quorum_factor=1&page_size=100&page=${page}&apikey=8ebb100f8dbd94b032393de929156869`,
+        type: "GET",
+        dataType: "jsonp",
+        jsonpCallback: "addRandomTrackCallback"
+    });
+}
+
+function addRandomTrackCallback(json) {
+    debugger;
+    var trackList = json.message.body.track_list;
+
+    let trackIndex = getRandomInt(trackList.length);
+    let track = trackList[trackIndex];
+
+    randomSongs.push(track);
+    window.songsRetrieved++;
+    if (window.songsRetrieved < window.retrieveSongcount)
+        randomSongList();
+};
+
+// Mozilla example of getting interger between 1 and max (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
