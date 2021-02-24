112c6970cdb656e0a6a6202795efc2fc72063223
Ben Kelly <benskelly10@gmail.com>
2021-02-24 09:11:05 +0000

    Add array of countries for chart section

---
+++ assets/js/countries.js
@@ -0,0 +1,10 @@
+var countries = [
+    {"UK": "gb"},
+    {"USA": "us"},
+    {"Ireland": "ie"},
+    {"France": "fr"},
+    {"Germany": "de"},
+    {"India": "in"},
+    {"Spain": "es"},
+    {"Japan": "jp"}
+]
