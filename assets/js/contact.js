11ca3da9b35b5b6b6626ac75fb20c780b09189d9
Ben Kelly <benskelly10@gmail.com>
2021-03-03 19:00:27 +0000

    Add contact.html with emailjs service

---
+++ assets/js/contact.js
@@ -0,0 +1,14 @@
+
+$("#email-contact-form").on("submit", function(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_90gdmfq', 'template_hy9y9uj', '#email-contact-form')
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert("Your email was successfully sent!");
+        }, function(error) {
+            console.log('FAILED...', error);
+            alert("Uh oh! There was a problem sending your email! Please try again.");
+        });
+
+})
